7cd8fad598676b1d67e167d33f124fb61a84ad86
Yash Desai <yash.desai.naik@gmail.com>
2023-10-30 12:26:49 +0530

    static login page

---
+++ src/Login.tsx
@@ -0,0 +1,60 @@
+// Login.tsx
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Perform your login logic here
+    // If login is successful, navigate to another page
+    navigate("/dashboard"); // Change this to the appropriate route
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full p-2 border rounded-md"
+              placeholder="Your Email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full p-2 border rounded-md"
+              placeholder="Your Password"
+            />
+          </div>
+          <button
+            type="button"
+            onClick={handleLogin}
+            className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
