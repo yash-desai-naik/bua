4f812c5807aca7fa3e7220e8ff591ffcbd463722
yashdesai <yash.desai.naik@gmail.com>
2023-12-02 07:06:14 +0530

    editable PPT

--- src/PPT.tsx
+++ src/PPT.tsx
@@ -1,4 +1,7 @@
 import pptxgen from "pptxgenjs";
+import { useState } from "react";
+
+import data from "./data.json";
 
 //centimeter to inches
 function cmToIn(cm: number) {
@@ -6,84 +9,150 @@ function cmToIn(cm: number) {
 }
 
 const PPT = () => {
+  const [bands, setBands] = useState<
+    {
+      band: string;
+      bandH: number;
+      bandW: number;
+      bandX: number;
+      bandY: number;
+    }[]
+  >();
   function makePPT() {
     // 1. Create a new Presentation
-    let pres = new pptxgen();
+    let pptx = new pptxgen();
+
+    // Define new layout for the Presentation
+    pptx.defineLayout({ name: "A3", width: 16.5, height: 11.7 });
+
+    // Set presentation to use new layout
+    pptx.layout = "A3";
 
     // 2. Add a Slide
-    let slide = pres.addSlide();
+    let slide = pptx.addSlide();
 
     // 3. Add one or more objects (Tables, Shapes, Images, Text and Media) to the Slide
     let textboxText =
       "Emerging Relativity of DRs of CEO & COO - Observations & Analysis";
-    let textboxOpts = { y: cmToIn(0.4), color: "006550" };
+    let textboxOpts = { y: cmToIn(0.3), color: "006550" };
     slide.addText(textboxText, textboxOpts);
 
-    // // Shapes without text
-    // slide.addShape(pres.ShapeType.rect, { fill: { color: "FF0000" } });
-    // slide.addShape(pres.ShapeType.ellipse, {
-    //   fill: { type: "solid", color: "0088CC" },
-    // });
-    // slide.addShape(pres.ShapeType.line, {
-    //   line: { color: "FF0000", width: 1 },
-    // });
+    const bs = data.map((d) => ({ band: d.band }));
 
-    // // Shapes with text
-    // slide.addText("ShapeType.rect", {
-    //   shape: pres.ShapeType.rect,
-    //   fill: { color: "FF0000" },
-    // });
-    // slide.addText("ShapeType.ellipse", {
-    //   shape: pres.ShapeType.ellipse,
-    //   fill: { color: "FF0000" },
-    // });
-    // slide.addText("ShapeType.line", {
-    //   shape: pres.ShapeType.line,
-    //   line: { color: "FF0000", width: 1, dashType: "lgDash" },
-    // });
+    setBands(
+      bs.reverse().map((band, index) => ({
+        band: band.band,
 
-    [{ band: "L1" }, { band: "L2" }, { band: "L3" }, { band: "L4" }]
-      .reverse()
-      .map((band, index) => {
-        slide.addText(band.band, {
-          fill: { color: "006550" },
-          // x: cmToIn(0.5),
-          y: cmToIn(13.2 - cmToIn(index * (1.5 + 2.4))),
-          w: cmToIn(1.5),
-          h: cmToIn(0.25),
-          rotate: 270,
-          fontSize: 8,
-          align: "center",
-          wrap: false,
-          color: "FFFFFF",
-        });
-      });
+        bandH: cmToIn(26.73 / bs.length),
+        bandW: cmToIn(1),
+        bandX: cmToIn(0.5),
+        bandY: cmToIn(26.73 - cmToIn(index * 8)),
+      }))
+    );
 
-    
-      slide.addText('CEO\n(L1)\n\n1200', {
-        shape:pres.ShapeType.rect,
-        fill: { color: "FFFFFF", type:"none" },
-        line:{ width:2, color:'A9A9A9' },
+    bands?.map((band, index) => {
+      slide.addText(band.band, {
+        x: band.bandX,
+        y: band.bandY,
+        w: band.bandW,
+        h: band.bandH,
 
-       
-        x: cmToIn(1.5),
-        y: cmToIn(10.2),
-        w: cmToIn(2.5),
-        h: cmToIn(2.5),
         fontSize: 12,
         align: "center",
         wrap: false,
-        
-      })
+        // color: "FFFFFF",
+      });
+    });
+
+    //  // x: cmToIn(0.5),
+    //  y: cmToIn(13.2 - cmToIn(index * (1.5 + 2.4))),
+    //  w: cmToIn(1.5),
+    //  h: cmToIn(0.25),
+
+    // const bands = [
+    //   { band: "L1", n: 3 },
+    //   { band: "L2", n: 1 },
+    //   { band: "L3", n: 4 },
+    //   { band: "L4", n: 2 },
+    // ];
+    // let totalOccupiedHeight = 0;
+    // bands.reverse().map((band, index) => {
+    //   totalOccupiedHeight += cmToIn((12 * band.n * 0.2) / bands.length);
+    //   slide.addText(band.band, {
+    //     fill: { color: "006550" },
+    //     x: cmToIn(0.5),
+    //     y: cmToIn(12 - cmToIn(index * (1.5 + 6.4))),
+    //     w: cmToIn(0.25),
+    //     h: cmToIn((12 * band.n * 0.2) / bands.length),
+    //     // rotate: 270,
+    //     fontSize: 8,
+    //     align: "center",
+    //     wrap: false,
+    //     color: "FFFFFF",
+    //   });
+    //   // slide.addText(band.band, {
+    //   //   fill: { color: "006550" },
+    //   //   x: cmToIn(0.5),
+    //   //   y: cmToIn(12.2 - cmToIn(index * (1.5 + 6.4))),
+    //   //   w: cmToIn(0.25),
+    //   //   h: cmToIn((12 * band.n * 0.2) / bands.length),
+    //   //   // rotate: 270,
+    //   //   fontSize: 8,
+    //   //   align: "center",
+    //   //   wrap: false,
+    //   //   color: "FFFFFF",
+    //   // });
+    // });
+
+    bands?.forEach((band) =>
+      data
+        .filter((d) => d.band === band.band)
+        .map((d1) =>
+          d1.uniqueJobs.forEach((job, i) =>
+            slide.addText(
+              `${job.title}\n(${job.current_grade})\n${job.hayScore}\n`,
+              {
+                shape: pptx.ShapeType.rect,
+                fill: { color: "FFFFFF", type: "none" },
+                line: { width: 2, color: "A9A9A9" },
+                // x: band.bandX*band.bandW + d1.uniqueJobs.length,
+                x: 1 + band.bandX + i * 3.04,
+                y:
+                  band.bandY +
+                  1 -
+                  job.hayScore /
+                    Math.max(...d1.uniqueJobs.map((o) => o.hayScore)),
+                w: cmToIn(2 * job.title.length * 0.1),
+                h: cmToIn(1.8),
+                fontSize: 12,
+                align: "center",
+                wrap: true,
+                fit: "resize",
+              }
+            )
+          )
+        )
+    );
+
+    slide.addShape(pptx.ShapeType.line, {
+      line: { color: "FF0000", width: 1 },
+      points: [
+        { x: cmToIn(3.04), y: cmToIn(7.83) },
+        { x: cmToIn(10.76), y: cmToIn(20.43) },
+      ],
+    });
 
     // 4. Save the Presentation
-    pres.writeFile();
+    pptx.writeFile();
   }
 
   return (
     <>
       <div>
-        <button onClick={makePPT}>Make PPT</button>
+        <pre className="text-xs">{JSON.stringify(bands, null, 2)}</pre>
+        <button onClick={makePPT} className="bg-black p-1 text-gray-50">
+          Make PPT
+        </button>
       </div>
     </>
   );
